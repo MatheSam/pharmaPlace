6a9b7281a3709852b2de99b22867662a8bfc1266
Matheus Sam <matheus.snavarro@outlook.com>
2022-07-05 20:19:32 -0300

    feature: created modals dashboard

--- app/src/Pages/Dashboard/index.jsx
+++ app/src/Pages/Dashboard/index.jsx
@@ -1,5 +1,15 @@
+import ModalAdd from "../../Components/Dashboard/Modals/Add";
+import ModalDelete from "../../Components/Dashboard/Modals/Delete";
+import ModalEdit from "../../Components/Dashboard/Modals/Edit";
+
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <ModalEdit />
+      <ModalAdd />
+      <ModalDelete />
+    </>
+  );
 };
 
 export default Dashboard;
