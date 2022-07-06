e8d629e2b0a47deb13fc7b1749691e98d52428ad
matheuszeiser <matheuszaizer@hotmail.com>
2022-07-05 22:11:35 -0300

    Feat: Componente Footer criado

--- app/src/Components/Footer/index.jsx
+++ app/src/Components/Footer/index.jsx
@@ -1,5 +1,37 @@
+import { ThemeFooter } from "./style";
+import { BsLinkedin } from "react-icons/bs";
+import { RiCopyrightFill } from "react-icons/ri";
+import { MdOutlineContactMail } from "react-icons/md";
+
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <ThemeFooter>
+      <h3><MdOutlineContactMail/> Contato</h3>
+      <ul>
+        <li>
+          <BsLinkedin />Francisco Jeihson
+        </li>
+        <li>
+          <BsLinkedin /> Gabriel Luz
+        </li>
+        <li>
+          <BsLinkedin /> Julio César
+        </li>
+        <li>
+          <BsLinkedin /> Lucas Ribeiro
+        </li>
+        <li>
+          <BsLinkedin /> Matheus Sam
+        </li>
+        <li>
+          <BsLinkedin /> Matheus Zeiser
+        </li>
+      </ul>
+      <span>
+        <RiCopyrightFill /> Copyright direitos reservados. 2022.
+      </span>
+    </ThemeFooter>
+  );
 };
 
 export default Footer;
