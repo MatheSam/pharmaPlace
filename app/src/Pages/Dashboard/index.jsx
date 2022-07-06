844eaeafc2f60d47d3214dddee0d7eaa7f143cdf
Francisco Jeihson <jeihson_alvesso@hotmail.com>
2022-07-05 20:56:53 -0400

    feat: created cards products dashboard

--- app/src/Pages/Dashboard/index.jsx
+++ app/src/Pages/Dashboard/index.jsx
@@ -1,5 +1,12 @@
+import CardsDashboard from "../../Components/Dashboard/Cards-dashboard";
+
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <CardsDashboard />
+    </>
+  );
 };
 
 export default Dashboard;
