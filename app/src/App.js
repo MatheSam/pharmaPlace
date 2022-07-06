8688324529994985b789cb37dc50fad9f3957cfe
Matheus Sam <matheus.snavarro@outlook.com>
2022-07-06 16:35:40 -0300

    fix: somes bugs

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,24 @@
 import Rotas from "./Routes/router";
 import GlobalStyle from "./Styles/global";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Rotas />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 }
