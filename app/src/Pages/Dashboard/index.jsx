832c6d0664831ac7cb8615d31b2a74d06c795296
Matheus Sam <matheus.snavarro@outlook.com>
2022-07-08 15:21:32 -0300

    just att

--- app/src/Pages/Dashboard/index.jsx
+++ app/src/Pages/Dashboard/index.jsx
@@ -12,11 +12,11 @@ import { useContext } from "react";
 
 const Dashboard = () => {
   const {
-      products,
-      inputFilterFunction, 
-      setInputValue, 
-      setFilteredProductsDash 
-    } = useContext(ProductsContext);
+    products,
+    inputFilterFunction,
+    setInputValue,
+    setFilteredProductsDash,
+  } = useContext(ProductsContext);
 
   const id = JSON.parse(localStorage.getItem("@userData")).id;
   const pharmaProducts = products.filter((prod) => prod.userId === id);
@@ -24,15 +24,13 @@ const Dashboard = () => {
   return (
     <>
       <Header />
-<<<<<<< HEAD
 
       <SearchInput
         set={setInputValue}
-        func={() => inputFilterFunction(pharmaProducts, setFilteredProductsDash)}
+        func={() =>
+          inputFilterFunction(pharmaProducts, setFilteredProductsDash)
+        }
       />
-=======
-      <SearchInput />
->>>>>>> 762c9eb7a8e43708ec4ab9e945cb8e05782c90e1
       <SectionBox>
         <ModalAdd />
       </SectionBox>
