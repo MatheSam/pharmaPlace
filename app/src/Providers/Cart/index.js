a50b176e783546b829dafc33fef60e52a151eb6b
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-07-10 20:33:20 -0300

    fix: tirando imports não utilizados no cart provider

--- app/src/Providers/Cart/index.js
+++ app/src/Providers/Cart/index.js
@@ -1,5 +1,4 @@
-import { createContext, useEffect, useState } from "react";
-import api from "../../services/api";
+import { createContext, useState } from "react";
 import React from "react";
 
 export const CartContext = createContext();
