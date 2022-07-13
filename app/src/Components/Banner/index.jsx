92800bd4782292981ea5a2cd7431470e14721fb4
Júlio César Oliveira <juliocomp99@hotmail.com>
2022-07-13 12:50:32 -0300

    feat [banners extras]: implementação de dois banners

--- app/src/Components/Banner/index.jsx
+++ app/src/Components/Banner/index.jsx
@@ -1,5 +1,7 @@
 import { Container } from "./style";
 import Pharmacist from "../../Assets/LottieJson/Pharmacist.json";
+import Shipping from "../../Assets/LottieJson/Shipping.json";
+import Sale from "../../Assets/LottieJson/Sale.json";
 import Lottie from "react-lottie";
 import { useNavigate } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,16 +11,28 @@ import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import "./swiper.css";
 
-const BannerArea = () => {
+const BannerArea = (route) => {
   const navigate = useNavigate();
   const handleClick = () => {
-    return navigate("/registerSeller");
+    return navigate(route);
   };
 
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: Pharmacist,
+  };
+
+  const defaultOptionsTwo = {
+    loop: true,
+    autoplay: true,
+    animationData: Shipping,
+  };
+
+  const defaultOptionsThree = {
+    loop: true,
+    autoplay: true,
+    animationData: Sale,
   };
 
   const settings = {
@@ -46,67 +60,83 @@ const BannerArea = () => {
             </section>
             <section className="text_three">
               <p>Cadastro simplificado e gratuito</p>
-              <button onClick={handleClick} className="button_visit">
+              <button
+                onClick={() => handleClick("/registerSeller")}
+                className="button_visit"
+              >
                 Cadastrar
               </button>
             </section>
           </div>
           <section className="animation">
-            <Lottie options={defaultOptions} />
+            <Lottie options={defaultOptions} width="600px" height="600px" />
           </section>
         </Container>
       </SwiperSlide>
+
       <SwiperSlide>
         <Container>
-          <div className="box">
+          <div className="box second">
             <section className="text_one">
               <h3>
-                Cadastre sua empresa e receba milhares de visitas na Pharma
-                Place
+                Frete Grátis para todo o Brasil em compras a partir de R$ 99,90
               </h3>
             </section>
             <section className="text_two">
               <p>
-                O lugar certo para obter mais visibilidade e aumentar as vendas
-                com o menor custo
+                Acompanhe o andamento do seu pedido bem como o rastreamento de
+                forma simplificada
               </p>
             </section>
             <section className="text_three">
-              <p>Cadastro simplificado e gratuito</p>
-              <button onClick={handleClick} className="button_visit">
+              <p>Faça seu cadastro e não perca</p>
+              <button
+                onClick={() => handleClick("/registerUser")}
+                className="button_visit"
+              >
                 Cadastrar
               </button>
             </section>
           </div>
           <section className="animation">
-            <Lottie options={defaultOptions} />
+            <Lottie
+              options={defaultOptionsTwo}
+              width="450px"
+              height="450px"
+              margin-left="20px"
+            />
           </section>
         </Container>
       </SwiperSlide>
+
       <SwiperSlide>
         <Container>
-          <div className="box">
+          <div className="box three">
             <section className="text_one">
-              <h3>
-                Cadastre sua empresa e receba milhares de visitas na Pharma
-                Place
-              </h3>
+              <h3>Todo o site com 10% OFF e Frete Grátis na primeira compra</h3>
             </section>
             <section className="text_two">
               <p>
-                O lugar certo para obter mais visibilidade e aumentar as vendas
-                com o menor custo
+                Cadastre-se e receba 10% OFF na sua primeira compra em qualquer
+                uma de nossas farmácias parceiras
               </p>
             </section>
             <section className="text_three">
               <p>Cadastro simplificado e gratuito</p>
-              <button onClick={handleClick} className="button_visit">
+              <button
+                onClick={() => handleClick("/registerUser")}
+                className="button_visit"
+              >
                 Cadastrar
               </button>
             </section>
           </div>
           <section className="animation">
-            <Lottie options={defaultOptions} />
+            <Lottie
+              options={defaultOptionsThree}
+              width="600px"
+              height="600px"
+            />
           </section>
         </Container>
       </SwiperSlide>
