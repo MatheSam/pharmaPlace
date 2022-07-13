4f5092d1d085b198d633090a6d95af88b476969a
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-07-13 02:28:16 -0300

    feat: slide criado

--- app/src/Components/Banner/index.jsx
+++ app/src/Components/Banner/index.jsx
@@ -2,6 +2,12 @@ import { Container } from "./style";
 import Pharmacist from "../../Assets/LottieJson/Pharmacist.json";
 import Lottie from "react-lottie";
 import { useNavigate } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, A11y, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import "./swiper.css";
 
 const BannerArea = () => {
   const navigate = useNavigate();
@@ -14,31 +20,97 @@ const BannerArea = () => {
     autoplay: true,
     animationData: Pharmacist,
   };
+
+  const settings = {
+    navigation: true,
+    pagination: { clickable: true },
+    autoplay: true,
+    loop: true,
+  };
   return (
-    <Container>
-      <div className="box">
-        <section className="text_one">
-          <h3>
-            Cadastre sua empresa e receba milhares de visitas na Pharma Place
-          </h3>
-        </section>
-        <section className="text_two">
-          <p>
-            O lugar certo para obter mais visibilidade e aumentar as vendas com
-            o menor custo
-          </p>
-        </section>
-        <section className="text_three">
-          <p>Cadastro simplificado e gratuito</p>
-          <button onClick={handleClick} className="button_visit">
-            Cadastrar
-          </button>
-        </section>
-      </div>
-      <section className="animation">
-        <Lottie options={defaultOptions} />
-      </section>
-    </Container>
+    <Swiper modules={[Navigation, Pagination, A11y, Autoplay]} {...settings}>
+      <SwiperSlide>
+        <Container>
+          <div className="box">
+            <section className="text_one">
+              <h3>
+                Cadastre sua empresa e receba milhares de visitas na Pharma
+                Place
+              </h3>
+            </section>
+            <section className="text_two">
+              <p>
+                O lugar certo para obter mais visibilidade e aumentar as vendas
+                com o menor custo
+              </p>
+            </section>
+            <section className="text_three">
+              <p>Cadastro simplificado e gratuito</p>
+              <button onClick={handleClick} className="button_visit">
+                Cadastrar
+              </button>
+            </section>
+          </div>
+          <section className="animation">
+            <Lottie options={defaultOptions} />
+          </section>
+        </Container>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Container>
+          <div className="box">
+            <section className="text_one">
+              <h3>
+                Cadastre sua empresa e receba milhares de visitas na Pharma
+                Place
+              </h3>
+            </section>
+            <section className="text_two">
+              <p>
+                O lugar certo para obter mais visibilidade e aumentar as vendas
+                com o menor custo
+              </p>
+            </section>
+            <section className="text_three">
+              <p>Cadastro simplificado e gratuito</p>
+              <button onClick={handleClick} className="button_visit">
+                Cadastrar
+              </button>
+            </section>
+          </div>
+          <section className="animation">
+            <Lottie options={defaultOptions} />
+          </section>
+        </Container>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Container>
+          <div className="box">
+            <section className="text_one">
+              <h3>
+                Cadastre sua empresa e receba milhares de visitas na Pharma
+                Place
+              </h3>
+            </section>
+            <section className="text_two">
+              <p>
+                O lugar certo para obter mais visibilidade e aumentar as vendas
+                com o menor custo
+              </p>
+            </section>
+            <section className="text_three">
+              <p>Cadastro simplificado e gratuito</p>
+              <button onClick={handleClick} className="button_visit">
+                Cadastrar
+              </button>
+            </section>
+          </div>
+          <section className="animation">
+            <Lottie options={defaultOptions} />
+          </section>
+        </Container>
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
